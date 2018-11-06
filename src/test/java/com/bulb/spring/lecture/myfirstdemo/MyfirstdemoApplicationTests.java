f1c1594423ea69016d1e7ec8eae2993955faebd2
ivanpavlina <ivan.pavlina0@gmail.com>
2018-11-06 12:12:14 +0100

    initial

---
+++ src/test/java/com/bulb/spring/lecture/myfirstdemo/MyfirstdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bulb.spring.lecture.myfirstdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyfirstdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
